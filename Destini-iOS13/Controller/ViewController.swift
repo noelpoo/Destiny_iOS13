67ea81d70e52e083d0e74a6ac6273238c66df210
Noel Poo <poonsk@garena.com>
2020-08-26 12:04:52 +0800

    new project

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -2,8 +2,6 @@
 //  ViewController.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
@@ -13,12 +11,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+        
+    var storyBrain = storyModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
+    }
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+                
+        storyBrain.nextStory(input: sender.currentTitle!)
+        updateUI()
+        
+    }
+    
+    
+    @objc func updateUI() {
+        storyLabel.text = storyBrain.stories[storyBrain.storyNum].title
+        choice1Button.setTitle(storyBrain.stories[storyBrain.storyNum].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyBrain.storyNum].choice2, for: .normal)
+        
+        
     }
 
 
-}
 
+
+}
